98448fc4fc45fb775a00a6b0e5c94055bca4253b
domsin <dominicksinicropi@server.geneland.private>
2015-05-09 16:31:35 -0700

    added a comment to test commits

--- Postcard3/ViewController.swift
+++ Postcard3/ViewController.swift
@@ -29,6 +29,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //Code will run when we press Send Mail button
+        //Add a comment here to test commits
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
